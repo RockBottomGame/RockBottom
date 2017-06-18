d5b90e33d6be7bdcd4a15588e3227ba3f186d084
Ellpeck <megamaximal@gmail.com>
2017-06-18 19:12:37 +0200

    remove the stupid info box on the title screen

--- src/main/java/de/ellpeck/rockbottom/gui/menu/GuiMainMenu.java
+++ src/main/java/de/ellpeck/rockbottom/gui/menu/GuiMainMenu.java
@@ -1,18 +1,14 @@
 package de.ellpeck.rockbottom.gui.menu;
 
+import de.ellpeck.rockbottom.RockBottom;
 import de.ellpeck.rockbottom.api.IGameInstance;
 import de.ellpeck.rockbottom.api.assets.IAssetManager;
-import de.ellpeck.rockbottom.api.assets.font.FormattingCode;
 import de.ellpeck.rockbottom.api.gui.Gui;
 import de.ellpeck.rockbottom.api.gui.IGuiManager;
 import de.ellpeck.rockbottom.api.gui.component.ComponentButton;
-import de.ellpeck.rockbottom.RockBottom;
-import de.ellpeck.rockbottom.gui.component.ComponentMessageBox;
 import org.newdawn.slick.Graphics;
 
 public class GuiMainMenu extends Gui{
-
-    private static boolean infoBox;
 
     public GuiMainMenu(){
         super(100, 100);
@@ -37,11 +33,6 @@ public class GuiMainMenu extends Gui{
 
         this.components.add(new ComponentButton(this, 4, width-47, 2, 45, 10, assetManager.localize(RockBottom.internalRes("button.credits"))));
         this.components.add(new ComponentButton(this, 5, width-47, 14, 45, 10, assetManager.localize(RockBottom.internalRes("button.mods"))));
-
-        if(!infoBox){
-            this.components.add(new ComponentMessageBox(this, 6, this.guiLeft+this.sizeX/2-75, this.guiTop+this.sizeY/2-25, 150, 50, 0.25F, FormattingCode.YELLOW+"Hello! \nThis is Rock Bottom by Ellpeck! \nYou have been given a super alpha copy of this game (that's what this is) to test out and tell me what you think about it. \nI'd really appreciate if you were to give the game a go and, while you try out everything there is to try at the moment (which is probably like 20-30 minutes of actual stuff to do), make a list of things you like, dislike or that you want added, removed or changed. Any questions or bug reports you have should also go on that list. \nWhen you're done, just give it to me in some way. \nWhile you do this, though, note that there is a lot of stuff planned for the game and that everything you see and do is subject to mild or heavy changes, however, every item you see is craftable and both copper and coal generate in the world. There might also be some easter eggs. \nAnyway, &rthanks a lot, and have fun! <3"));
-            infoBox = true;
-        }
     }
 
     @Override
@@ -87,10 +78,6 @@ public class GuiMainMenu extends Gui{
         }
         else if(button == 5){
             guiManager.openGui(new GuiMods(this));
-            return true;
-        }
-        else if(button == 6){
-            this.components.remove(6);
             return true;
         }
         else{
