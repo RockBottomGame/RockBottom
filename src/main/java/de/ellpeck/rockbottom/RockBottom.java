2dbe8959319b2c5a63f014f54ab72d8d7994ac20
Ellpeck <megamaximal@gmail.com>
2017-06-25 15:21:56 +0200

    fix version stuff :V

--- src/main/java/de/ellpeck/rockbottom/RockBottom.java
+++ src/main/java/de/ellpeck/rockbottom/RockBottom.java
@@ -57,7 +57,7 @@ import java.util.UUID;
 
 public class RockBottom extends BasicGame implements IGameInstance{
 
-    public static final String VERSION = "@VERSION@";
+    public static final String VERSION = "0.0.7";
     public static final String NAME = "Rock Bottom";
     public static final String ID = "rockbottom";
 
