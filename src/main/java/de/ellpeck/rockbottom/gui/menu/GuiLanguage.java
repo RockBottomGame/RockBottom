d23eca5f3b22a3bd496831493470c4d4ff11ca58
Ellpeck <megamaximal@gmail.com>
2017-09-02 15:48:17 +0200

    implemented polish and made the language menu look a bit cleaner

--- src/main/java/de/ellpeck/rockbottom/gui/menu/GuiLanguage.java
+++ src/main/java/de/ellpeck/rockbottom/gui/menu/GuiLanguage.java
@@ -7,6 +7,8 @@ import de.ellpeck.rockbottom.api.assets.local.AssetLocale;
 import de.ellpeck.rockbottom.api.assets.local.Locale;
 import de.ellpeck.rockbottom.api.gui.Gui;
 import de.ellpeck.rockbottom.api.gui.component.ComponentButton;
+import de.ellpeck.rockbottom.api.gui.component.ComponentScrollMenu;
+import de.ellpeck.rockbottom.api.util.BoundBox;
 import de.ellpeck.rockbottom.api.util.reg.IResourceName;
 import de.ellpeck.rockbottom.init.AbstractGame;
 
@@ -15,20 +17,23 @@ import java.util.Map;
 public class GuiLanguage extends Gui{
 
     public GuiLanguage(Gui parent){
-        super(304, 150, parent);
+        super(150, 150, parent);
     }
 
     @Override
     public void initGui(IGameInstance game){
         super.initGui(game);
 
-        IAssetManager manager = game.getAssetManager();
+        BoundBox area = new BoundBox(0, 0, 150, 106).add(this.guiLeft, this.guiTop);
+        ComponentScrollMenu menu = new ComponentScrollMenu(this, this.guiLeft-8, this.guiTop, 106, 1, 6, area);
+        this.components.add(menu);
 
-        int i = 0;
+        IAssetManager manager = game.getAssetManager();
         for(Map.Entry<IResourceName, AssetLocale> entry : manager.getAllOfType(AssetLocale.class).entrySet()){
             IResourceName res = entry.getKey();
             Locale loc = entry.getValue().get();
-            this.components.add(new ComponentButton(this, this.guiLeft+this.sizeX/2-75, this.guiTop+(i*20), 150, 16, () -> {
+
+            menu.add(new ComponentButton(this, 0, 0, 150, 16, () -> {
                 if(manager.getLocale() != loc){
                     game.getSettings().currentLocale = res.toString();
                     game.getDataManager().savePropSettings(game.getSettings());
@@ -39,8 +44,9 @@ public class GuiLanguage extends Gui{
                 }
                 return false;
             }, loc.localize(null, res)));
-            i++;
         }
+
+        menu.organize();
 
         this.components.add(new ComponentButton(this, this.guiLeft+this.sizeX/2-40, this.guiTop+this.sizeY-16, 80, 16, () -> {
             game.getGuiManager().openGui(this.parent);
