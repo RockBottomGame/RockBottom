af84025f7505328d524f72107a8d1a6ae0451caf
ellpeck <megamaximal@gmail.com>
2017-09-23 15:08:35 +0100

    bump version

--- src/main/java/de/ellpeck/rockbottom/init/AbstractGame.java
+++ src/main/java/de/ellpeck/rockbottom/init/AbstractGame.java
@@ -33,7 +33,7 @@ import java.util.function.Supplier;
 
 public abstract class AbstractGame implements IGameInstance{
 
-    public static final String VERSION = "0.1.0";
+    public static final String VERSION = "0.1.1";
     public static final String NAME = "Rock Bottom";
     public static final String ID = "rockbottom";
     private static final int INTERVAL = 1000/Constants.TARGET_TPS;
