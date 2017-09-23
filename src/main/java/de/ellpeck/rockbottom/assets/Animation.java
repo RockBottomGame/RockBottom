46dc8afa3f88873f49c0200eeb03aeeaf9cd4308
ellpeck <megamaximal@gmail.com>
2017-09-23 22:44:19 +0100

    remove copyright headers that snuck in from the API

--- src/main/java/de/ellpeck/rockbottom/assets/Animation.java
+++ src/main/java/de/ellpeck/rockbottom/assets/Animation.java
@@ -1,24 +1,3 @@
-/*
- * This file ("Animation.java") is part of the RockBottomAPI by Ellpeck.
- * View the source code at <https://github.com/RockBottomGame/>.
- * View information on the project at <https://rockbottom.ellpeck.de/>.
- *
- * The RockBottomAPI is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * The RockBottomAPI is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with the RockBottomAPI. If not, see <http://www.gnu.org/licenses/>.
- *
- * © 2017 Ellpeck
- */
-
 package de.ellpeck.rockbottom.assets;
 
 import com.google.gson.JsonArray;
@@ -121,7 +100,7 @@ public class Animation implements IAnimation{
 
     @Override
     public void drawRow(long startTimeMillis, int row, float x1, float y1, float x2, float y2, float srcX1, float srcY1, float srcX2, float srcY2, int[] light, int filter){
-        long time = startTimeMillis >= 0 ? Util.getTimeMillis()-startTimeMillis : Util.getTimeMillis();
+        long time = startTimeMillis > 0 ? Util.getTimeMillis()-startTimeMillis : Util.getTimeMillis();
         long runningTime = time%this.getTotalTime(row);
 
         int accum = 0;
