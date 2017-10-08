093dbecaf4e6582ff7338594c54cf0bfd427e0ea
Ellpeck <megamaximal@gmail.com>
2017-10-08 16:19:21 +0200

    make the world info save when the server creates it

--- src/main/java/de/ellpeck/rockbottom/init/RockBottomServer.java
+++ src/main/java/de/ellpeck/rockbottom/init/RockBottomServer.java
@@ -89,6 +89,7 @@ public class RockBottomServer extends AbstractGame{
         }
         else{
             info.seed = Util.RANDOM.nextLong();
+            info.save();
             isNew = true;
         }
 
