7ebd099f21444d6a51875c9238a2b7c0ca330431
Ellpeck <megamaximal@gmail.com>
2017-11-28 18:40:13 +0100

    added a system for tiles to not have certain state combinations. Also added a way to get props via strings rather than their objects

---
+++ src/main/java/de/ellpeck/rockbottom/world/gen/feature/trees/TreeDesigns.java
@@ -0,0 +1,33 @@
+package de.ellpeck.rockbottom.world.gen.feature.trees;
+
+public final class TreeDesigns{
+
+    public static final String[][][] TREE_DESIGNS = new String[][][]{
+            {
+                    {" LLL "},
+                    {"LLLLL"},
+                    {" LLL "},
+                    {" `^´ "},
+                    {"  T  "},
+                    {" <v> "}
+            },
+            {
+                    {" L "},
+                    {"LLL"},
+                    {" L "},
+                    {" ^ "},
+                    {" T "},
+                    {" T "},
+                    {"<v>"}
+            },
+            {
+                    {" LLLLL "},
+                    {"LLLLLLL"},
+                    {" LLLLL "},
+                    {"  `^´  "},
+                    {"   T   "},
+                    {"  <v>  "}
+            }
+    };
+
+}
