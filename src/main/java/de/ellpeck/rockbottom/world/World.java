09ac60c0e86c77e9ace30ac862ae224d1a624907
Ellpeck <megamaximal@gmail.com>
2017-12-12 16:59:32 +0100

    made light around the player a bit less bright

--- src/main/java/de/ellpeck/rockbottom/world/World.java
+++ src/main/java/de/ellpeck/rockbottom/world/World.java
@@ -292,8 +292,8 @@ public class World implements IWorld{
         if(!game.isDedicatedServer()){
             AbstractEntityPlayer player = game.getPlayer();
             double dist = Util.distanceSq(x+0.5D, y, player.x, player.y);
-            if(dist <= 25D){
-                byte newLight = (byte)(0.3D*(25D-dist));
+            if(dist <= 15D){
+                byte newLight = (byte)(0.2D*(15D-dist));
                 if(light < newLight){
                     light = newLight;
                 }
