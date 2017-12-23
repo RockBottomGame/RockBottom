0ba00e44f4e079cf592dc2b88b6fc0162ff5dd8d
ellpeck <megamaximal@gmail.com>
2017-12-23 02:58:29 +0100

    0.1.9

--- src/main/java/de/ellpeck/rockbottom/init/AbstractGame.java
+++ src/main/java/de/ellpeck/rockbottom/init/AbstractGame.java
@@ -35,7 +35,7 @@ import java.util.function.Predicate;
 
 public abstract class AbstractGame implements IGameInstance{
 
-    public static final String VERSION = "0.1.8";
+    public static final String VERSION = "0.1.9";
     public static final String NAME = "Rock Bottom";
     public static final String ID = "rockbottom";
     private static final int INTERVAL = 1000/Constants.TARGET_TPS;
