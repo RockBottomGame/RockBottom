dc6a592b65e361ee215962d03372267894f0ca9f
ellpeck <megamaximal@gmail.com>
2017-12-30 21:12:54 +0100

    made the goo fall

--- src/main/java/de/ellpeck/rockbottom/world/tile/TileRemainsGoo.java
+++ src/main/java/de/ellpeck/rockbottom/world/tile/TileRemainsGoo.java
@@ -1,5 +1,6 @@
 package de.ellpeck.rockbottom.world.tile;
 
+import de.ellpeck.rockbottom.api.GameContent;
 import de.ellpeck.rockbottom.api.RockBottomAPI;
 import de.ellpeck.rockbottom.api.tile.TileBasic;
 import de.ellpeck.rockbottom.api.world.IWorld;
@@ -28,6 +29,31 @@ public class TileRemainsGoo extends TileBasic{
 
     @Override
     public boolean canBreak(IWorld world, int x, int y, TileLayer layer){
+        return false;
+    }
+
+    @Override
+    public void onChangeAround(IWorld world, int x, int y, TileLayer layer, int changedX, int changedY, TileLayer changedLayer){
+        if(this.fall(world, x, y, layer, true)){
+            world.scheduleUpdate(x, y, layer, 10);
+        }
+    }
+
+    @Override
+    public void onScheduledUpdate(IWorld world, int x, int y, TileLayer layer, int scheduledMeta){
+        this.fall(world, x, y, layer, false);
+    }
+
+    private boolean fall(IWorld world, int x, int y, TileLayer layer, boolean simulate){
+        if(!world.getState(x, y-1).getTile().isFullTile()){
+            if(world.getState(layer, x, y-1).getTile().canReplace(world, x, y-1, layer)){
+                if(!simulate){
+                    world.setState(layer, x, y-1, world.getState(layer, x, y));
+                    world.setState(layer, x, y, GameContent.TILE_AIR.getDefState());
+                }
+                return true;
+            }
+        }
         return false;
     }
 
