f7555687bdbf8db90d34e8cfd5ef66bdf78e535a
Ellpeck <megamaximal@gmail.com>
2018-01-28 12:00:16 +0100

    changed the tile inventory handling a bit as the old way was a bit of a mess

--- src/main/java/de/ellpeck/rockbottom/world/tile/entity/TileEntityChest.java
+++ src/main/java/de/ellpeck/rockbottom/world/tile/entity/TileEntityChest.java
@@ -2,44 +2,28 @@ package de.ellpeck.rockbottom.world.tile.entity;
 
 import de.ellpeck.rockbottom.api.RockBottomAPI;
 import de.ellpeck.rockbottom.api.data.set.DataSet;
-import de.ellpeck.rockbottom.api.inventory.IInventory;
-import de.ellpeck.rockbottom.api.inventory.Inventory;
-import de.ellpeck.rockbottom.api.inventory.TileInventory;
-import de.ellpeck.rockbottom.api.item.ItemInstance;
-import de.ellpeck.rockbottom.api.tile.entity.IInventoryHolder;
+import de.ellpeck.rockbottom.api.tile.entity.FilteredInventory;
 import de.ellpeck.rockbottom.api.tile.entity.TileEntity;
-import de.ellpeck.rockbottom.api.util.Direction;
+import de.ellpeck.rockbottom.api.tile.entity.TileInventory;
 import de.ellpeck.rockbottom.api.util.Util;
 import de.ellpeck.rockbottom.api.world.IWorld;
 import de.ellpeck.rockbottom.api.world.layer.TileLayer;
 import de.ellpeck.rockbottom.net.packet.toclient.PacketChestOpen;
 
 import java.util.Collections;
-import java.util.List;
 
-public class TileEntityChest extends TileEntity implements IInventoryHolder{
+public class TileEntityChest extends TileEntity{
 
     private int openCount;
-    private final Inventory inventory = new TileInventory(this, 20);
-    private final List<Integer> inputOutputSlots = Collections.unmodifiableList(Util.makeIntList(0, this.inventory.getSlotAmount()));
+    private final TileInventory inventory = new TileInventory(this, 20, Collections.unmodifiableList(Util.makeIntList(0, this.inventory.getSlotAmount())));
 
     public TileEntityChest(IWorld world, int x, int y, TileLayer layer){
         super(world, x, y, layer);
     }
 
     @Override
-    public IInventory getInventory(){
+    public FilteredInventory getInventory(){
         return this.inventory;
-    }
-
-    @Override
-    public List<Integer> getInputSlots(ItemInstance instance, Direction dir){
-        return this.inputOutputSlots;
-    }
-
-    @Override
-    public List<Integer> getOutputSlots(Direction dir){
-        return this.inputOutputSlots;
     }
 
     @Override
