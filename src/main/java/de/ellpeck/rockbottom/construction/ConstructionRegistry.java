2e5554336c8ae48e9e33e310854661fda1a982e3
Ellpeck <megamaximal@gmail.com>
2018-02-09 02:01:16 +0100

    added construction recipes for doors and boards

--- src/main/java/de/ellpeck/rockbottom/construction/ConstructionRegistry.java
+++ src/main/java/de/ellpeck/rockbottom/construction/ConstructionRegistry.java
@@ -18,17 +18,19 @@ public final class ConstructionRegistry{
     public static IRecipe chest;
 
     public static void init(){
-        BRITTLE_TOOLS.add(new KnowledgeBasedRecipe(new ItemInstance(GameContent.ITEM_BRITTLE_PICKAXE), new ItemUseInfo(GameContent.TILE_PEBBLES, 12), new ItemUseInfo(GameContent.TILE_LOG, 2)).registerManual());
-        BRITTLE_TOOLS.add(new KnowledgeBasedRecipe(new ItemInstance(GameContent.ITEM_BRITTLE_AXE), new ItemUseInfo(GameContent.TILE_PEBBLES, 10), new ItemUseInfo(GameContent.TILE_LOG, 2)).registerManual());
-        BRITTLE_TOOLS.add(new KnowledgeBasedRecipe(new ItemInstance(GameContent.ITEM_BRITTLE_SHOVEL), new ItemUseInfo(GameContent.TILE_PEBBLES, 8), new ItemUseInfo(GameContent.TILE_LOG, 2)).registerManual());
+        BRITTLE_TOOLS.add(new KnowledgeBasedRecipe(new ItemInstance(GameContent.ITEM_BRITTLE_PICKAXE), new ItemUseInfo(GameContent.TILE_PEBBLES, 12), new ItemUseInfo(GameContent.WOOD_BOARDS, 4)).registerManual());
+        BRITTLE_TOOLS.add(new KnowledgeBasedRecipe(new ItemInstance(GameContent.ITEM_BRITTLE_AXE), new ItemUseInfo(GameContent.TILE_PEBBLES, 10), new ItemUseInfo(GameContent.WOOD_BOARDS, 4)).registerManual());
+        BRITTLE_TOOLS.add(new KnowledgeBasedRecipe(new ItemInstance(GameContent.ITEM_BRITTLE_SHOVEL), new ItemUseInfo(GameContent.TILE_PEBBLES, 8), new ItemUseInfo(GameContent.WOOD_BOARDS, 4)).registerManual());
 
-        torch = new KnowledgeBasedRecipe(new ItemInstance(GameContent.TILE_TORCH, 3), new ItemUseInfo(GameContent.TILE_COAL, 1), new ItemUseInfo(GameContent.TILE_LOG, 1)).registerManual();
+        torch = new KnowledgeBasedRecipe(new ItemInstance(GameContent.TILE_TORCH, 3), new ItemUseInfo(GameContent.TILE_COAL, 1), new ItemUseInfo(GameContent.WOOD_BOARDS, 1)).registerManual();
 
-        ladder = new KnowledgeBasedRecipe(new ItemInstance(GameContent.TILE_LADDER, 5), new ItemUseInfo(GameContent.TILE_LOG, 4)).registerManual();
+        ladder = new KnowledgeBasedRecipe(new ItemInstance(GameContent.TILE_LADDER, 5), new ItemUseInfo(GameContent.WOOD_BOARDS, 8)).registerManual();
 
-        chest = new KnowledgeBasedRecipe(new ItemInstance(GameContent.TILE_CHEST), new ItemUseInfo(GameContent.TILE_LOG, 25)).registerManual();
+        chest = new KnowledgeBasedRecipe(new ItemInstance(GameContent.TILE_CHEST), new ItemUseInfo(GameContent.WOOD_BOARDS, 20), new ItemUseInfo(GameContent.TILE_LOG, 4)).registerManual();
 
-        new BasicRecipe(new ItemInstance(GameContent.TILE_SIGN), new ItemUseInfo(GameContent.TILE_LOG, 4)).registerManual();
+        new BasicRecipe(new ItemInstance(GameContent.TILE_SIGN), new ItemUseInfo(GameContent.WOOD_BOARDS, 8)).registerManual();
+        new BasicRecipe(new ItemInstance(GameContent.WOOD_BOARDS, 3), new ItemUseInfo(GameContent.TILE_LOG)).registerManual();
+        new BasicRecipe(new ItemInstance(GameContent.TILE_WOOD_DOOR), new ItemUseInfo(GameContent.WOOD_BOARDS, 16)).registerManual();
     }
 
 }
