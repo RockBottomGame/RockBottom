8729a91d47faf176f5b4f281cccf61520596e059
Ellpeck <megamaximal@gmail.com>
2018-05-06 18:42:11 +0200

    made grass tufts render in the foreground

--- src/main/java/de/ellpeck/rockbottom/render/tile/TileGrassTuftRenderer.java
+++ src/main/java/de/ellpeck/rockbottom/render/tile/TileGrassTuftRenderer.java
@@ -15,6 +15,19 @@ public class TileGrassTuftRenderer extends TileMetaRenderer<TileGrassTuft>{
 
     @Override
     public void render(IGameInstance game, IAssetManager manager, IRenderer g, IWorld world, TileGrassTuft tile, TileState state, int x, int y, TileLayer layer, float renderX, float renderY, float scale, int[] light){
+        if(layer != TileLayer.MAIN){
+            this.doRender(manager, world, tile, state, x, y, renderX, renderY, scale, light);
+        }
+    }
+
+    @Override
+    public void renderInForeground(IGameInstance game, IAssetManager manager, IRenderer g, IWorld world, TileGrassTuft tile, TileState state, int x, int y, TileLayer layer, float renderX, float renderY, float scale, int[] light){
+        if(layer == TileLayer.MAIN){
+            this.doRender(manager, world, tile, state, x, y, renderX, renderY, scale, light);
+        }
+    }
+
+    private void doRender(IAssetManager manager, IWorld world, TileGrassTuft tile, TileState state, int x, int y, float renderX, float renderY, float scale, int[] light){
         float topX = renderX+(float)Math.sin((world.getTotalTime()+(x*50))/25D%(2*Math.PI))*scale/15F;
         ITexture texture = this.getTexture(manager, tile, state.get(tile.metaProp)).getPositionalVariation(x, y);
         texture.draw(topX, renderY, renderX, renderY+scale, renderX+scale, renderY+scale, topX+scale, renderY, 0, 0, texture.getRenderWidth(), texture.getRenderHeight(), light, Colors.WHITE);
