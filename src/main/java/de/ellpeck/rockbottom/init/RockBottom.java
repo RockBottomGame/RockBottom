d1261713fa67da40cf021157113fb01611295da6
Ellpeck <megamaximal@gmail.com>
2018-05-06 10:55:15 +0200

    GAH NO

--- src/main/java/de/ellpeck/rockbottom/init/RockBottom.java
+++ src/main/java/de/ellpeck/rockbottom/init/RockBottom.java
@@ -102,7 +102,6 @@ public class RockBottom extends AbstractGame{
 
     @Override
     public void init(){
-        this.uniqueId = UUID.randomUUID();
         RockBottomAPI.logger().info("Initializing GLFW");
 
         GLFW.glfwSetErrorCallback(this.errorCallback);
