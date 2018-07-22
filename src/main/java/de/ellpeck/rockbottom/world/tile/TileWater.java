d7c829d34c1502b208b04f7fac7b76c11ae74e8c
Ellpeck <megamaximal@gmail.com>
2018-07-22 20:14:52 +0200

    make small amounts of water evaporate over time

--- src/main/java/de/ellpeck/rockbottom/world/tile/TileWater.java
+++ src/main/java/de/ellpeck/rockbottom/world/tile/TileWater.java
@@ -1,9 +1,12 @@
 package de.ellpeck.rockbottom.world.tile;
 
+import de.ellpeck.rockbottom.api.GameContent;
 import de.ellpeck.rockbottom.api.entity.Entity;
 import de.ellpeck.rockbottom.api.tile.TileLiquid;
 import de.ellpeck.rockbottom.api.tile.state.TileState;
 import de.ellpeck.rockbottom.api.util.BoundBox;
+import de.ellpeck.rockbottom.api.util.Direction;
+import de.ellpeck.rockbottom.api.util.Util;
 import de.ellpeck.rockbottom.api.util.reg.ResourceName;
 import de.ellpeck.rockbottom.api.world.IWorld;
 import de.ellpeck.rockbottom.api.world.layer.TileLayer;
@@ -42,6 +45,27 @@ public class TileWater extends TileLiquid {
                     }
 
                     break;
+                }
+            }
+        }
+    }
+
+    @Override
+    public void updateRandomly(IWorld world, int x, int y, TileLayer layer) {
+        if (Util.RANDOM.nextFloat() >= 0.95F) {
+            TileState state = world.getState(layer, x, y);
+            int level = state.get(this.level);
+            if (level <= 3) {
+                for (Direction dir : Direction.ADJACENT) {
+                    if (!world.isPosLoaded(x + dir.x, y + dir.y) || world.getState(layer, x + dir.x, y + dir.y).getTile() == this) {
+                        return;
+                    }
+                }
+
+                if (level <= 0) {
+                    world.setState(layer, x, y, GameContent.TILE_AIR.getDefState());
+                } else {
+                    world.setState(layer, x, y, state.prop(this.level, level - 1));
                 }
             }
         }
