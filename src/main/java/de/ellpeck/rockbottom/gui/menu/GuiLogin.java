aae89eacb1c9e8b44cada0696b2f8bd6f6c9d31a
Raph Hennessy <raph.hennessy@gmail.com>
2019-08-19 23:35:37 +1000

    Login with email

--- src/main/java/de/ellpeck/rockbottom/gui/menu/GuiLogin.java
+++ src/main/java/de/ellpeck/rockbottom/gui/menu/GuiLogin.java
@@ -21,11 +21,12 @@ import java.util.UUID;
 
 public class GuiLogin extends Gui {
 
-    private ComponentInputField nameField;
+    private ComponentInputField emailField;
     private ComponentInputField passField;
     private ComponentButton loginButton;
     private ComponentButton logoutButton;
 
+    private boolean loggedIn;
     private ResourceName feedback;
 
     public GuiLogin(Gui parent) {
@@ -38,8 +39,9 @@ public class GuiLogin extends Gui {
         IGuiManager guiManager = game.getGuiManager();
         IAssetManager assetManager = game.getAssetManager();
 
-        this.nameField = new ComponentInputField(this, this.width / 2 - 75, 25, 150, 16, true, true, false, 256, false);
-        this.components.add(this.nameField);
+
+        this.emailField = new ComponentInputField(this, this.width / 2 - 75, 25, 150, 16, true, true, false, 256, false);
+        this.components.add(this.emailField);
 
         this.passField = new ComponentInputField(this, this.width / 2 - 75, 55, 150, 16, true, true, false, 256, false);
         this.passField.setCensored(true);
@@ -47,18 +49,17 @@ public class GuiLogin extends Gui {
 
         this.loginButton = new ComponentButton(this, this.width / 2 - 50, 80, 100, 16, () -> {
             Thread thread = new Thread(() -> {
-                JsonObject obj = PostUtil.post("https://canitzp.de:38000/login", new PostData("mode", "login"), new PostData("username", this.nameField.getText()), new PostData("password", this.passField.getText()));
+                JsonObject obj = PostUtil.post("https://canitzp.de:38000/login", new PostData("mode", "login"), new PostData("email", this.emailField.getText()), new PostData("password", this.passField.getText()));
                 if (obj.has("code")) {
                     int code = obj.get("code").getAsInt();
                     if (code == 100) {
-                        UserAccount account = new UserAccount(UUID.fromString(obj.get("uuid").getAsString()), this.nameField.getText(), UUID.fromString(obj.get("token").getAsString()));
+                        UserAccount account = new UserAccount(UUID.fromString(obj.get("uuid").getAsString()), this.emailField.getText(), UUID.fromString(obj.get("token").getAsString()));
                         account.cache();
                         game.loginAs(account);
 
                         boolean firstLogin = obj.get("was_verified").getAsBoolean();
                         if (firstLogin) {
                             RockBottomAPI.logger().info("Logged into account " + account.getUsername() + " for the first time!");
-                            account.changePassword(this.passField.getText(), "ster2002");
                         }
                     }
                 }
@@ -88,7 +89,7 @@ public class GuiLogin extends Gui {
         }, assetManager.localize(ResourceName.intern("button.logout")));
         this.components.add(this.logoutButton);
 
-        this.updateButtons();
+        this.updateButtons(game);
 
         this.components.add(new ComponentButton(this, this.width / 2 - 40, this.height - 30, 80, 16, () -> {
             guiManager.openGui(this.parent);
@@ -101,20 +102,22 @@ public class GuiLogin extends Gui {
         super.render(game, manager, g);
 
         IFont font = manager.getFont();
-        font.drawCenteredString(this.width / 2, 15, manager.localize(ResourceName.intern("info.username")), 0.35F, false);
-        font.drawCenteredString(this.width / 2, 45, manager.localize(ResourceName.intern("info.password")), 0.35F, false);
-
+        if (!this.loggedIn) {
+            font.drawCenteredString(this.width / 2, 15, manager.localize(ResourceName.intern("info.account.email")), 0.35F, false);
+            font.drawCenteredString(this.width / 2, 45, manager.localize(ResourceName.intern("info.account.password")), 0.35F, false);
+        }
         if (feedback != null) {
             font.drawCenteredString(this.width / 2, 120, manager.localize(feedback), 0.35F, false);
         }
     }
 
-    private void updateButtons() {
-        boolean loggedIn = false;
-        this.nameField.setActive(!loggedIn);
-        this.passField.setActive(!loggedIn);
-        this.loginButton.setActive(!loggedIn);
-        this.logoutButton.setActive(loggedIn);
+    private void updateButtons(IGameInstance game) {
+        this.loggedIn = game.getAccount() != null;
+
+        this.emailField.setActive(!this.loggedIn);
+        this.passField.setActive(!this.loggedIn);
+        this.loginButton.setActive(!this.loggedIn);
+        this.logoutButton.setActive(this.loggedIn);
     }
 
     @Override
