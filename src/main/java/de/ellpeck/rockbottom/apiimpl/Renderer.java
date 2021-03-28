c76b3997ec0eebb5db2997aeeffb1a7720867fee
Quarris <tomasz.winnicki@live.co.uk>
2021-03-28 21:41:39 +0100

    ...

--- src/main/java/de/ellpeck/rockbottom/apiimpl/Renderer.java
+++ src/main/java/de/ellpeck/rockbottom/apiimpl/Renderer.java
@@ -23,7 +23,6 @@ import de.ellpeck.rockbottom.api.render.engine.IVBO;
 import de.ellpeck.rockbottom.api.render.engine.TextureBank;
 import de.ellpeck.rockbottom.api.render.item.IItemRenderer;
 import de.ellpeck.rockbottom.api.util.Colors;
-import de.ellpeck.rockbottom.api.util.DrawContext;
 import de.ellpeck.rockbottom.api.util.Util;
 import de.ellpeck.rockbottom.api.util.reg.ResourceName;
 import de.ellpeck.rockbottom.assets.font.Font;
@@ -110,21 +109,6 @@ public class Renderer implements IRenderer {
     public void setDefaultProgram(IShaderProgram defaultProgram) {
         this.defaultProgram = defaultProgram;
         this.setProgram(null);
-    }
-
-    @Override
-    public void addTexturedRegion(ITexture texture, DrawContext ctx) {
-        this.setTexture(texture);
-
-        float u = ctx.uv().getU();
-        float v = ctx.uv().getV();
-        float u2 = ctx.uv().getU2();
-        float v2 = ctx.uv().getV2();
-
-        int topLeft = this.combineLight(ctx.light, ITexture.TOP_LEFT, ctx.filter);
-        int bottomLeft = this.combineLight(ctx.light, ITexture.BOTTOM_LEFT, ctx.filter);
-        int bottomRight = this.combineLight(ctx.light, ITexture.BOTTOM_RIGHT, ctx.filter);
-        int topRight = this.combineLight(ctx.light, ITexture.TOP_RIGHT, ctx.filter);
     }
 
     @Override
